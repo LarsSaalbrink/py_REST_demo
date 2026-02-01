4be826c5163b46a205f8e05ec4f11cb696711ab8
Lars Saalbrink <larssaalbrink@gmail.com>
2026-02-01 12:42:57 +0100

    Design login page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
+            <h1>Sign in</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "50px",
+                        padding: "40px",
+                        backgroundColor: "#908164",
+                        borderRadius: "20px",
+                    }}
+                >
+                    <input
+                        className="loginInput"
+                        type="text"
+                        placeholder="Username"
+                    />
+                    <input
+                        className="loginInput"
+                        type="password"
+                        placeholder="Password"
+                    />
+                </div>
+                <button
+                    className="loginButton"
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    Continue
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
